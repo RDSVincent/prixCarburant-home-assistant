3dc1e2206f63a76255d3055767d19c757701923b
RDSVincent <37441277+RDSVincent@users.noreply.github.com>
2022-04-22 20:36:21 +0200

    Update sensor.py

--- custom_components/myPrixCarburant/sensor.py
+++ custom_components/myPrixCarburant/sensor.py
@@ -193,7 +193,7 @@ class MyPrixCarburant_(Entity):
         return self._icon
 
     @property
-    def device_state_attributes(self):
+    def extra_state_attributes(self):
         """Return the device state attributes of the last update."""
 
         attrs = {
